beedb4002d3b019c5a5e47c97085dca25a24ba19
David Jeong <davidjeong@DavidJeongMegazoneCloud.local>
2024-04-16 14:33:41 -0700

    made minor changes to output.tf and readme.md

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Scalable-Wordpress-Architecture
 Create Scalable Wordpress Architecture using Terraform/AWS resources
+
+This project was greatly inspired by the work of Adrian Cantrill: [See his hands-on lab here](https://github.com/acantril/learn-cantrill-io-labs/tree/master/aws-elastic-wordpress-evolution)
+
+<img width="900" alt="image" src="https://github.com/djeong95/Scalable-Wordpress-Architecture/assets/102641321/eb39cd1b-0c17-4d36-9e13-060e227c4857">
+
--- outputs.tf
+++ outputs.tf
@@ -3,12 +3,12 @@
 #   value       = { for vpc in aws_vpc.this : vpc.tags.Name => { "cidr_block" : vpc.cidr_block, "id" : vpc.id } }
 # }
 
-output "db_username" {
-    value = nonsensitive(aws_db_instance.this.username)
-    description = "The username used for the database instance."
+output "alb_endpoint" {
+    value = nonsensitive(aws_lb.this.dns_name)
+    description = "The endpoint for the application load balancer."
 }
 
 output "db_endpoint" {
     value = nonsensitive(aws_db_instance.this.endpoint)
-    description = "The username used for the database instance."
+    description = "The endpoint for the database instance."
 }
